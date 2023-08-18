150b91234e83cc529f1f1dd21917ff5e193cbc9a
romulo.banares <romulo88@hotmail.com>
2023-08-18 16:48:40 +0200

    add mark down parser and post styles

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,8 +1,11 @@
+import Head from 'next/head';
+import Date from '../../components/date';
 import Layout from '../../components/layout';
 import { getAllPostIds, getPostData } from '../../lib/posts';
+import utilStyles from '../../styles/utils.module.css';
 
 export async function getStaticProps({ params }) {
-  const postData = getPostData(params.id);
+  const postData = await getPostData(params.id);
   return {
     props: {
       postData,
@@ -21,11 +24,17 @@ export async function getStaticPaths() {
 export default function Post({ postData }) {
   return (
     <Layout>
-      {postData.title}
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <h1 className={utilStyles.headingXl}>{postData.title}</h1>
       <br />
       {postData.id}
       <br />
-      {postData.date}
+      <div className={utilStyles.lightText}>
+        <Date dateString={postData.date} />
+      </div>
+      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
     </Layout>
   );
 }
