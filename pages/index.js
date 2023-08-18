1d4b0031dec64021d2ba9152199b40020ee2fe7b
romulo.banares <romulo88@hotmail.com>
2023-08-18 17:55:30 +0200

    change name

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hola, me llamo Rómulo y soy desarrollador front-end.</p>
+        <p>Hola, me llamo Robin y soy desarrollador front-end.</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
