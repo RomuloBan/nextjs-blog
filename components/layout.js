101a89f0419a6be2755e1fc190483703c740b8eb
romulo.banares <romulo88@hotmail.com>
2023-08-18 18:15:48 +0200

    fix name

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import styles from './layout.module.css';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
-const name = 'Romulo Bañares';
+const name = 'Rómulo Bañares';
 export const siteTitle = 'Next.js Sample Website';
 
 export default function Layout({ children, home }) {
