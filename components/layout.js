04131f913a65b3c12915dceb809810767103ca5c
romulo.banares <romulo88@hotmail.com>
2023-08-18 18:13:18 +0200

    change name

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import styles from './layout.module.css';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
-const name = 'Robin Hood';
+const name = 'Romulo Bañares';
 export const siteTitle = 'Next.js Sample Website';
 
 export default function Layout({ children, home }) {
